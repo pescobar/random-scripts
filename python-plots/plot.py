e7060a59f742617e054e20a06db2dbd5b8636923
escobar <escobar@kb-raphael.pz.unibas.ch>
2015-07-03 12:13:08 +0200

    added procs and threads. Added legends and colors

--- python-plots/plot.py
+++ python-plots/plot.py
@@ -4,29 +4,47 @@
 import matplotlib.pyplot as plt
 
 def main():
-    time, memory = parse_file('memory_run1.log')
+    time, memory, procs, threads = parse_file('memory_run1.log')
     #print time
     #print memory
-    plt.ylabel('memory consumption')
-    #plt.plot(memory, time)
-    plt.plot(time, memory)
+    #print memory
+    #print procs
+    #print threads
+    #plt.ylabel('memory consumption')
+    #plt.plot(time,memory, 'r--', time, procs, 'bs', time, threads, 'g^')
+    #plt.plot(time, memory)
+    line1, = plt.plot(time, memory, 'k--', label='Memory (GB)', color='green')
+    line2, = plt.plot(time, procs, 'k:', label='Num Procs', color = 'red')
+    line3, = plt.plot(time, threads, 'k:', label='Num Treads', color = 'blue')
+    plt.ticklabel_format(style='plain', axis='y')
+    #legend = plt.legend(loc='upper center', shadow=True)
+    legend = plt.legend()
     plt.show()
 
 def parse_file(file):
 
     time_values = []
     memory_values = []
+    num_procs = []
+    num_threads = []
     
     f = open(file)
     for l in f.readlines():
         if l.startswith('time'):
             time = l.split()[0].split(":")[1]
             memory = l.split()[1].split(":")[1]
+            # memory is in MB so divide by 1024 and round it so the 
+            # graph is easier to read. +-1G of ram is fine...
+            memory = round(float(memory) / 1024)
+            numprocs = l.split()[3].split(":")[1]
+            numthreads = l.split()[4].split(":")[1]
             #print time, memory
             time_values.append(time)
             memory_values.append(memory)
+            num_procs.append(numprocs)
+            num_threads.append(numthreads)
     f.close()
-    return time_values, memory_values
+    return time_values, memory_values, num_procs, num_threads
 
 if __name__ == "__main__":
     main()
